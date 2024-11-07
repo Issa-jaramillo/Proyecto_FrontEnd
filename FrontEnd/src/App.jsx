4a383a67b91cb3b1699eca2301cf1dbc0e1ab185
issa <issakaterinejaramilloalvarez@gmail.com>
2024-11-07 10:57:38 -0500

    tailwind configurado

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,13 @@
 
-
 function App() {
-
-
   return (
-    <>
-    hola
-    </>
-  )
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        ¡Bienvenido a mi portafolio!
+      </h1>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
