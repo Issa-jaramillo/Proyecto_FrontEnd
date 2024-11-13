f62392754f2001f697f3a144d7b3a5e042b4f84b
issa <issakaterinejaramilloalvarez@gmail.com>
2024-11-13 07:18:57 -0500

    hice ya la logica, solo falta arreglos de css

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,28 @@
 
+import Navbar from "./components/Navbar"
+import Me from "./components/Me"
+import About from "./components/About"
+import Technologies from "./components/Technologies"
+import Projects from "./components/Proyects"
+import Contact from "./components/Contact"
+
 function App() {
+
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-500">
-        ¡Bienvenido a mi portafolio!
-      </h1>
-      
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>   
+      </div>
+    <div className="container mx-auto px-8">
+      <Navbar/>
+      <Me/>
+       <About/> 
+      <Projects/>
+      <Technologies/>
+      <Contact/>
     </div>
-  );
+ </div>
+  )
 }
 
-export default App;
+export default App
---
+++ FrontEnd/src/constants/Index.jsx
@@ -0,0 +1,37 @@
+
+
+export const ME_CONTENT = "El desarrollo web front-end se refiere a la práctica de construir y diseñar la interfaz de usuario de un sitio web o aplicación. Esto implica trabajar con tecnologías del lado del cliente, como HTML, CSS y JavaScript, para crear páginas web interactivas y visualmente atractivas.";
+
+export const ABOUT_TEXT = "FRONT AND";
+export const PROYECTS = [
+    {
+        title: "PinkPanther Indumentaria ",
+        image:"../assets/img-proyect/foto.jpg" ,
+        description:
+          "",
+       technologies: ["html"] ,
+        link: ""
+      },
+      {
+        title: "Drivers App",
+       image: "../assets/img-proyect/foto.jpg" ,
+        description:
+          "Aplicación que tenía como objetivo la construcción de una Single Page Application con funcionalidades de búsqueda,filtrados, ordenamiento y creación para gestionar datos de conductores de manera eficiente.",
+        technologies: ["html"],
+        link: "https://github.com/ivanaspinoso/cr-pi-drivers-main"
+      },
+      {
+        title: "Portfolio Diseño UX UI",
+        image:"../assets/img-proyect/foto.jpg" ,
+        description:
+          "Portfolio personal de diseño UX UI mostrando los proyectos, mis habilidades, y contacto.",
+        technologies: ["Figma"],
+        link: ""
+      },
+      
+];
+export const CONTACT = {
+  
+    phoneNo: "+57 3042112244",
+    email: "issakaterinejaramilloalvarez@gmail.com",
+};
